eb8f810e630d30cb22c92f66d860bc75d4aebc86
code sponge <spon9ec0de@gmail.com>
2022-07-28 17:46:21 +0900

    Fix: install the typings for react

--- src/components/common/Input/index.tsx
+++ src/components/common/Input/index.tsx
@@ -1,31 +1,37 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 interface PropsInput01 {
-    width?: string;
-    height?: string;
-    borderRadois?: string;
-    color?: string;
-    onChange?: () => void;
-    fontSize?: string;
-    placeholder?: string;
-    padding?: string;
-    margin?: string;
-    focusRef?: any;
+  width?: string;
+  height?: string;
+  borderRadius?: string;
+  color?: string;
+  onChange?: () => void;
+  fontSize?: string;
+  placeholder?: string;
+  padding?: string;
+  margin?: string;
+  focusRef?: any;
 }
 
 const CommonInput01 = styled.input<PropsInput01>`
-    width: ${(props: PropsInput01) => props.width};
-    height: ${(props: PropsInput01) => props.height};
-    color: ${(props: PropsInput01) => props.color};
-    font-size: ${(props: PropsInput01) => props.fontSize};
-    padding: ${(props: PropsInput01) => props.padding};
-    margin: ${(props: PropsInput01) => props.margin};
-`
+  width: ${(props: PropsInput01) => props.width};
+  height: ${(props: PropsInput01) => props.height};
+  color: ${(props: PropsInput01) => props.color};
+  font-size: ${(props: PropsInput01) => props.fontSize};
+  padding: ${(props: PropsInput01) => props.padding};
+  margin: ${(props: PropsInput01) => props.margin};
+`;
 
-export default function Input01 (props: PropsInput01) {
-    return (
-        <>
-            <CommonInput01 width={props.width} height={props.height} color={props.color} fontSize={props.fontSize} placeholder={props.placeholder}/>
-        </>
-    )
+export default function Input01(props: PropsInput01) {
+  return (
+    <>
+      <CommonInput01
+        width={props.width}
+        height={props.height}
+        color={props.color}
+        fontSize={props.fontSize}
+        placeholder={props.placeholder}
+      />
+    </>
+  );
 }
